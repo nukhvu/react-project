e60c99255a20533427108b84e801a4e87779c75a
nukh <nohagairbekov2009@gmail.com>
2025-01-07 11:58:08 +0300

    add css modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header copy 2";
 import Nav from "./components/Nav";
 import Profile from "./components/Profile";
+import Header from "./components/Header";
 
 let App = () => {
   return (
